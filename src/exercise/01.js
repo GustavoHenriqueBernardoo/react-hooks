a61c8d2d2f5828f2ab818bed246d91f93b8537f6
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-07-16 10:50:31 -0300

    react hooks

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,12 +3,17 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting(props) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+
+  console.log(props)
+
+  const [name, setName] = React.useState(props.initicialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    event.preventDefault()
+    setName(event.target.value)
   }
 
   return (
@@ -23,7 +28,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initicialName={"Gustavo"} />
 }
 
 export default App
